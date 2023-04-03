d62a6b09fea894dd9fc3de0654ed59607169fec4
jongwon0280 <whddnjs0280@naver.com>
2023-04-04 01:37:36 +0900

    category

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    path('category/<str:slug>/',views.categories_page),
     path('<int:pk>/',views.PostDetail.as_view()),
     path('',views.PostList.as_view()),
     #path('<int:pnum>/',views.single_post_page),
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 
 class PostList(ListView):
     model=Post
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count']=Post.objects.filter(category=None).count()
+        return context
+
 class PostDetail(DetailView):
     model=Post
 
@@ -23,3 +30,12 @@ def index(request):
     )
 
 
+def categories_page(request,slug):
+    category = Category.objects.get(slug=slug)
+    context={
+            'categories' : Category.objects.all(),
+            'categories_less_post_count' : Post.objects.filter(category=None).count(),
+            'category' : category,
+            'post_list' : Post.objects.filter(category=category)
+     }
+    return render(request,'blog/post_list.html',context)
